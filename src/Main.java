e83a4c6bd714fa72f829483b79b9e9b4e50db52f
zsmlnr <zsozsomlnr.96@gmail.com>
2021-02-11 11:20:02 +0100

    Add exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        String firstName = "Zsolt";
+        String lastName = "Molnar";
+        char myGender = 'M';
+        int myAge = 23;
+        double averageGrade = 3.0;
+        boolean married = false;
+        Date birthDate = new Date(851818210000L);
+
+        System.out.println("Meine name ist.: " + firstName + " " + lastName);
+        System.out.println("Meine geschlecht ist.: " + myGender);
+        System.out.println("Mein alter.: " + myAge);
+        System.out.println("Ich bin geboren.: " + birthDate);
+        System.out.println("Mein Notendurchschnitt.: " + averageGrade);
+        System.out.println("Ich bin verheiratet.: " + married);
+
+
     }
+
 }
